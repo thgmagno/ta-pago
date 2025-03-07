463a570eecf2aa9098c0d2599b246f6503fd1241
Thiago Magno <thgmgn@gmail.com>
2025-03-07 14:53:22 -0300

    update

--- env.ts
+++ env.ts
@@ -1,12 +1,11 @@
 import { z } from 'zod'
 
 const schema = z.object({
+  AUTH_URL: z.string().url(),
   AUTH_SECRET: z.string(),
   AUTH_GOOGLE_ID: z.string(),
   AUTH_GOOGLE_SECRET: z.string(),
-  AUTH_GITHUB_ID: z.string(),
-  AUTH_GITHUB_SECRET: z.string(),
-  DATABASE_URL: z.string().url(),
+  POSTGRES_PRISMA_URL: z.string().url(),
 })
 
 export const env = schema.parse(process.env)
