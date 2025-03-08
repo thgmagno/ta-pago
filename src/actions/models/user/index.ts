9668296d2b07ff4cd760e71b198302b56efdfcad
Thiago Magno <thgmgn@gmail.com>
2025-03-08 17:11:04 -0300

    update

--- src/actions/models/user/index.ts
+++ src/actions/models/user/index.ts
@@ -1,2 +1,1 @@
 export * from './deactivateAccount'
-export * from './signOut'
--- src/actions/models/user/signOut.ts
+++
@@ -1,7 +0,0 @@
-'use server'
-
-import { repositories } from '@/database/repositories'
-
-export async function signOut() {
-  return repositories.users.user.signOut()
-}
